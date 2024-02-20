ab03ba4ddc818512528e2e66b5b7ae441b07e16d
Fer1196 <rodifer1196@hotmail.com>
2024-02-19 22:36:59 -0500

    Add text in bill to customer clients

--- src/pages/DetailCar/DetailCar.tsx
+++ src/pages/DetailCar/DetailCar.tsx
@@ -1,14 +1,126 @@
 import { Cart } from "../../components/Cart/Carts";
 import { useCart } from "../../hooks/useCart";
 import { ProductCart } from "../../utils/interfaces/Product";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 import "./DetailCar.css";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "#01eac2",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "#01eac2",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#01eac2",
+      },
+      "&:hover fieldset": {
+        borderColor: "#01eac2",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#01eac2",
+      },
+      "& input": {
+        color: "#01eac2",
+      },
+      "& placeholder": {
+        color: "#01eac2",
+      },
+      "& .MuiFormLabel-root": {
+        color: "#01eac2", // Color del texto del placeholder cuando el TextField está inactivo
+      },
+      "& .MuiInputLabel-outlined.MuiInputLabel-shrink": {
+        transform: "translate(14px, -6px) scale(0.75)", // Ajuste para el texto del placeholder cuando el TextField está inactivo
+      },
+    },
+  },
+});
+
 export function DetailCar() {
   const { cart } = useCart();
   let total: number = 0;
+  const classes = useStyles();
   return (
     <>
       <Cart />
       <div className="cart">
+        <div className="dataCustomer">
+          {" "}
+          <div className="dataRow">
+            <h2 className="cart-title">Datos de Facturacion</h2>
+
+            <TextField
+              className={classes.root}
+              id="outlined-basic"
+              label="Cedula"
+              variant="outlined"
+              InputLabelProps={{
+                style: { color: "#01eac2" },
+              }}
+              InputProps={{
+                style: { color: "#01eac2" },
+              }}
+            />
+          </div>
+          <div className="dataRow">
+            <TextField
+              className={classes.root}
+              id="outlined-basic"
+              label="Nombre"
+              variant="outlined"
+              InputLabelProps={{
+                style: { color: "#01eac2" },
+              }}
+              InputProps={{
+                style: { color: "#01eac2" },
+              }}
+            />
+            <TextField
+              className={classes.root}
+              id="outlined-basic"
+              label="Apellido"
+              variant="outlined"
+              InputLabelProps={{
+                style: { color: "#01eac2" },
+              }}
+              InputProps={{
+                style: { color: "#01eac2" },
+              }}
+            />
+          </div>
+          <div className="dataRow">
+            <TextField
+              className={classes.root}
+              id="outlined-basic"
+              label="Direccion"
+              variant="outlined"
+              InputLabelProps={{
+                style: { color: "#01eac2" },
+              }}
+              InputProps={{
+                style: { color: "#01eac2" },
+              }}
+            />
+
+            <TextField
+              className={classes.root}
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              InputLabelProps={{
+                style: { color: "#01eac2" },
+              }}
+              InputProps={{
+                style: { color: "#01eac2" },
+              }}
+            />
+          </div>
+        </div>
+
         <h1 className="cart-title">Carrito a Pagar</h1>
 
         <div className="cart-title-row">
