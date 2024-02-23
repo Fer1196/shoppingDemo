c287c428aab3510d0689266cb53de2b926fb9856
Fer1196 <rodifer1196@hotmail.com>
2024-02-23 00:59:01 -0500

    ver 171

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import "./index.css";
 import { FiltersProvider } from "./context/filtersContext.tsx";
 import { Navigation } from "./routes/Navigation.tsx";
 import { CartProvider } from "./context/cartContext.tsx";
 import { Toaster } from "sonner";
+import ReactDOM from "react-dom";
 
 // ReactDOM.createRoot(document.getElementById("root")!).render(
 //   <>
