0a9b17bca4f9a3db7c38f172c15c829a6093f990
Fer1196 <rodifer1196@hotmail.com>
2024-08-10 21:42:15 -0500

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="footer">
       <h5>
-        <span>RODRIGUEZ FERNANDO</span> - TFM FINAL 2
+        <span>RODRIGUEZ FERNANDO</span> - TFM FINAL
       </h5>
     </footer>
   );
